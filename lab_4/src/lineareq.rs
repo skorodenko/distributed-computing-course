1e9f3dfc5c77eb00fd5a783c43dc85f552f98e28
skorodenko <72010957+skorodenko@users.noreply.github.com>
2023-11-02 21:23:30 +0200

    Lab 4 + Report

--- lab_4/src/lineareq.rs
+++ lab_4/src/lineareq.rs
@@ -1,41 +1,39 @@
-use std::sync::{Arc, Mutex};
 use rayon::prelude::*;
-use nalgebra::{DMatrix, DVector};
+use ndarray::prelude::*;
 
 
-pub fn gauss (mut m: DMatrix<f64>) -> DVector<f64> {
-    let nrows = m.shape().0;
+pub fn gauss (mut m: Array<f64, Dim<[usize; 2]>>) -> Array<f64, Dim<[usize; 1]>> {
+    let (nrows, ncols) = m.dim();
 
     // Прямий хід
-    let _m = Arc::new(Mutex::new(&mut m));
     (0..nrows).into_iter().for_each(|i| {
-        let tmp = *_m.lock().unwrap().index((i,i));
-        
-        (0..nrows).rev().into_iter().for_each(|j| {
-            _m.lock().unwrap()[(i,j)] /= tmp;            
+        (0..ncols).rev().into_iter().for_each(|j| {
+            m[[i,j]] /= m[[i,i]];            
         });
-        
-        (i+1..nrows).into_par_iter().for_each(|j| {
-            let tmp = *_m.lock().unwrap().index((j,i));
 
-            let _m = _m.clone();
-            (i..nrows).into_iter().for_each(|k| {
-                let mut mm = _m.lock().unwrap();
-                mm[(j,k)] -= tmp * (*mm.index((i,k)));
+        let _m = m.clone();
+        m
+            .slice_mut(s![i+1..nrows, ..])
+            .axis_iter_mut(Axis(0))
+            .into_par_iter()
+            .for_each(|mut row| {
+                let scale = row[i];
+                
+                (i..ncols).into_iter().for_each(|k| {
+                    row[k] -= scale * _m[[i,k]];
+                });
             });
-        });
     });
-    
-    
+
     // Зворотній хід
-    let mut xx = DVector::<f64>::zeros(nrows);
-    xx[nrows - 1] = *m.index((nrows - 1, nrows));
+    let mut xx = Array::<f64, _>::zeros(nrows);
+    xx[nrows - 1] = m[[nrows - 1, nrows]];
     
     (0..nrows-1).rev().into_iter().for_each(|i| {
-        xx[i] = *m.index((i, nrows));
+        xx[i] = m[[i, nrows]];
 
         (i+1..nrows).into_iter().for_each(|j| {
-            xx[i] -= m.index((i,j)) * xx[j];
+            xx[i] -= m[[i,j]] * xx[j];
         });
     });
     
--- lab_4/src/main.rs
+++ lab_4/src/main.rs
@@ -1,12 +1,12 @@
-use nalgebra::{DMatrix};
 use lab_4::lineareq::gauss;
+use ndarray::prelude::*;
 
 fn main() {
-    let m = DMatrix::from_row_slice(3, 4, &[
-        2.0, -1.0, 0.0, 0.0,
-        -1.0, 1.0, 4.0, 13.0,
-        1.0, 2.0, 3.0, 14.0
-    ]);
+    let m = array![
+        [8., 7., 3., 18.],
+        [-7., -4., -4., -11.],
+        [-6., 5.0, -4.0, -15.0],
+    ];
 
     println!("Input: {}", m);
     
